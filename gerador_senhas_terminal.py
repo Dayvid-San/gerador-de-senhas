e29a0504259654e17c303e8d31857d05189c87ff
Dayvid Santana <santana.dayvid@outlook.com>
2022-06-16 12:33:09 -0300

    add function somador_listas

--- gerador_senhas_terminal.py
+++ gerador_senhas_terminal.py
@@ -4,24 +4,42 @@ from string import punctuation
 from string import ascii_letters
 
 
-tipos_elementos = str(input('Quais os elementos devo colocar?\n1 - Números\n2 - Caracteres\n3 - Letras\n'))
-numero_elementos_senha = str(input('Qual a quantidade elementos na senha?\n'))
 
+def somador_listas(lista):
+    total
+    for i in lista:
+        total = total + i
+    
+    return total
 
 
 def geradorSenhas(numero):
+    tipos_elementos = str(input('Quais os elementos devo colocar?\n1 - Números\n2 - Caracteres\n3 - Letras\n'))
+    symbols = []
     #symbols = ascii_letters + digits + punctuation
     #symbols = digits if 1 and 2 and 3 else 
     if '3' in tipos_elementos:
-        symbols += ascii_letters
+        symbols.append(ascii_letters)
     if '2' in tipos_elementos:
-        symbols += punctuation
+        symbols.append(punctuation)
     if '1' in tipos_elementos:
-        symbols += digits
+        symbols.append(digits)
+    else:
+        print('Não entendi sua resposta.\nTente novamente!')
 
+    symbols = somador_listas(symbols)
     
     secure_random = random.SystemRandom()
-    password = "".join(secure_random.choice(symbols) for i in range(numero)) 
+    password = ''.join(secure_random.choice(symbols) for i in range(numero)) 
     print(password)
+'''
+    
+
+'''
+
+
+
+
+numero_elementos_senha = str(input('Qual a quantidade elementos na senha?\n'))
 
 geradorSenhas(numero_elementos_senha)
